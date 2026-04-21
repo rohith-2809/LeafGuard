faea9582ad38c0e08aec8961751a9581e9c2becc
Rohith Vittamraj <158805322+rohith-2809@users.noreply.github.com>
2026-04-21 11:05:04 +0530

    Add keras files along with the project

--- mern/models/User.js
+++ mern/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true }, 
     createdAt: { type: Date, default: Date.now }
 });
 
