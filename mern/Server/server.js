f76124efe94c2a861d118cbef48e7b6e1924c3c4
Rohith Vittamraj <158805322+rohith-2809@users.noreply.github.com>
2026-04-25 23:07:30 +0530

    feat(fix) : fixed image handling

--- mern/Server/server.js
+++ mern/Server/server.js
@@ -1,6 +1,6 @@
 // server.js
 const express = require("express");
-const mongoose      = require("mongoose");
+const mongoose = require("mongoose");
 const cors          = require("cors");
 const helmet        = require("helmet");
 const compression   = require("compression");
