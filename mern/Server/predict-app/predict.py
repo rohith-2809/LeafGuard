6bf8a319d92ef86ee11ff2fb3ad192cd7802fa28
Rohith Vittamraj <158805322+rohith-2809@users.noreply.github.com>
2026-06-17 09:15:04 +0530

    fix :  fixed OOM error

--- mern/Server/predict-app/predict.py
+++ mern/Server/predict-app/predict.py
@@ -10,7 +10,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image, ImageOps
 
-
 from gradio_client import Client, handle_file
 
 # ——— Configuration —————————————
